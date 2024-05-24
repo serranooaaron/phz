c5f5daf9416bceb07d19b9f7c81d330742e94170
Aaron Serrano <143615019+serranooaaron@users.noreply.github.com>
2024-05-24 10:47:03 -0300

    Add files via upload

---
+++ impresora/src/impresoraa/Piezas.java
@@ -0,0 +1,19 @@
+package impresoraa;
+
+
+
+
+// PIEZAS DERIVA EN IMPRESIONES
+
+public class Piezas {
+
+	public static void main(String[] args) {
+		int pieza_id;
+		String nombre_pieza;
+		int calidad;
+		String nombre_impresora;
+		char archivo_gcode;
+
+	}
+
+}
