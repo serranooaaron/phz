58979f638a484ccc2ae51615086b79fcc5541006
Aaron Serrano <143615019+serranooaaron@users.noreply.github.com>
2024-06-03 01:48:55 -0300

    LOS 3 ARCHIVOS SON LA BD ACTUALIZADA.

---
+++ esquema.sql
@@ -0,0 +1,117 @@
+-- MySQL dump 10.13  Distrib 8.0.37, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gestbd
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `impresiones`
+--
+
+DROP TABLE IF EXISTS `impresiones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `impresiones` (
+  `idimpresiones` int NOT NULL,
+  `idpieza` int NOT NULL,
+  `idrollo` int NOT NULL,
+  `fecha_imp` date DEFAULT NULL,
+  `tiempo` time DEFAULT NULL,
+  `peso` double DEFAULT NULL,
+  `costo_kwh` float DEFAULT NULL,
+  `costo_pieza` float DEFAULT NULL,
+  PRIMARY KEY (`idimpresiones`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `impresoras`
+--
+
+DROP TABLE IF EXISTS `impresoras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `impresoras` (
+  `idimpresora` int NOT NULL,
+  `nombre_impresora` varchar(45) DEFAULT NULL,
+  `marca` varchar(45) DEFAULT NULL,
+  `modelo` varchar(45) DEFAULT NULL,
+  `alto_imp` double DEFAULT NULL,
+  `ancho_imp` double DEFAULT NULL,
+  `largo_imp` double DEFAULT NULL,
+  `consumo_hora` float DEFAULT NULL,
+  `horas_uso` int DEFAULT NULL,
+  `mantenimiento` date DEFAULT NULL,
+  PRIMARY KEY (`idimpresora`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `materiales`
+--
+
+DROP TABLE IF EXISTS `materiales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materiales` (
+  `idmateriales` int NOT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idmateriales`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `piezas`
+--
+
+DROP TABLE IF EXISTS `piezas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `piezas` (
+  `idpiezas` int NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `calidad_impresion` varchar(45) DEFAULT NULL,
+  `idimpresora` int DEFAULT NULL,
+  `gcode_file` mediumblob,
+  PRIMARY KEY (`idpiezas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `rollos`
+--
+
+DROP TABLE IF EXISTS `rollos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rollos` (
+  `idRollos` int NOT NULL,
+  `idmaterial` int DEFAULT NULL,
+  `idcolor` int DEFAULT NULL,
+  `peso_gr` double DEFAULT NULL,
+  `costo` float DEFAULT NULL,
+  PRIMARY KEY (`idRollos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-03  1:42:27
